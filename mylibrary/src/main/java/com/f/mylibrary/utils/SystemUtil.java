8412114d18e84ccc3e4c85723c6aa1158cd092da
2804863883@qq.com <bin19940116zz123>
2019-02-13 14:54:57 +0800

    更新了SystemUtil

--- mylibrary/src/main/java/com/f/mylibrary/utils/SystemUtil.java
+++ mylibrary/src/main/java/com/f/mylibrary/utils/SystemUtil.java
@@ -2,9 +2,11 @@ package com.f.mylibrary.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.telephony.TelephonyManager;
 
 import java.util.Locale;
+import java.util.UUID;
 
 public class SystemUtil {
 
@@ -20,7 +22,7 @@ public class SystemUtil {
     /**
      * 获取当前系统上的语言列表(Locale列表)
      *
-     * @return  语言列表
+     * @return 语言列表
      */
     public static Locale[] getSystemLanguageList() {
         return Locale.getAvailableLocales();
@@ -29,7 +31,7 @@ public class SystemUtil {
     /**
      * 获取当前手机系统版本号
      *
-     * @return  系统版本号
+     * @return 系统版本号
      */
     public static String getSystemVersion() {
         return android.os.Build.VERSION.RELEASE;
@@ -38,7 +40,7 @@ public class SystemUtil {
     /**
      * 获取手机型号
      *
-     * @return  手机型号
+     * @return 手机型号
      */
     public static String getSystemModel() {
         return android.os.Build.MODEL;
@@ -47,7 +49,7 @@ public class SystemUtil {
     /**
      * 获取手机厂商
      *
-     * @return  手机厂商
+     * @return 手机厂商
      */
     public static String getDeviceBrand() {
         return android.os.Build.BRAND;
@@ -56,7 +58,7 @@ public class SystemUtil {
     /**
      * 获取手机IMEI(需要“android.permission.READ_PHONE_STATE”权限)
      *
-     * @return  手机IMEI
+     * @return 手机IMEI
      */
     public static String getIMEI(Context ctx) {
         TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Activity.TELEPHONY_SERVICE);
@@ -64,6 +66,39 @@ public class SystemUtil {
             return tm.getDeviceId();
         }
         return null;
+    }
+
+    /**
+     * 获得独一无二的Psuedo ID
+     **/
+    public static String getUniquePsuedoID() {
+        String serial = null;
+
+        String m_szDevIDShort = "99" +
+                Build.BOARD.length() % 10 + Build.BRAND.length() % 10 +
+
+                Build.CPU_ABI.length() % 10 + Build.DEVICE.length() % 10 +
+
+                Build.DISPLAY.length() % 10 + Build.HOST.length() % 10 +
+
+                Build.ID.length() % 10 + Build.MANUFACTURER.length() % 10 +
+
+                Build.MODEL.length() % 10 + Build.PRODUCT.length() % 10 +
+
+                Build.TAGS.length() % 10 + Build.TYPE.length() % 10 +
+
+                Build.USER.length() % 10; //13 位
+
+        try {
+            serial = android.os.Build.class.getField("SERIAL").get(null).toString();
+            //API>=9 使用serial号
+            return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+        } catch (Exception exception) {
+            //serial需要一个初始化
+            serial = "serial"; // 随便一个初始化
+        }
+        //使用硬件信息拼凑出来的15位号码
+        return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
     }
 
 }
